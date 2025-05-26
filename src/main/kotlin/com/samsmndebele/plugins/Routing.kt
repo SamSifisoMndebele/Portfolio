06b243d78a2f1f0cb6a4ce7295260608f99e0a7f
Sam Sifiso Mndebele <sams.mndebele@gmail.com>
2025-05-26 18:08:10 +0200

    Add contact to firebase firestore

--- src/main/kotlin/com/samsmndebele/plugins/Routing.kt
+++ src/main/kotlin/com/samsmndebele/plugins/Routing.kt
@@ -1,17 +1,14 @@
 package com.samsmndebele.plugins
 
+import com.samsmndebele.firebase.Firebase
 import com.samsmndebele.pages.indexHtml
 import com.samsmndebele.services.Contact
-import com.samsmndebele.services.ContactService
+import com.samsmndebele.utils.await
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.html.*
 import io.ktor.server.http.content.*
-import io.ktor.server.plugins.autohead.*
-import io.ktor.server.plugins.statuspages.*
 import io.ktor.server.request.*
-import io.ktor.server.resources.*
-import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
@@ -31,17 +28,17 @@ fun Application.configureRouting() {
                 val phone = parameters["phone"]
                 val message = parameters["message"]!!
 
-                ContactService.create(
-                    Contact(
-                        name = name,
-                        email = email,
-                        phone = phone,
-                        message = message
-                    )
+                val contact = Contact(
+                    name = name,
+                    email = email,
+                    phone = phone,
+                    message = message
                 )
+                Firebase.firestore.collection("contacts").add(contact).await()
+//                ContactService.create(contact)
 
                 call.respondHtml(HttpStatusCode.Created) { indexHtml(true) }
-            } catch (e: Exception) {
+            } catch (_: Exception) {
                 call.respondHtml(HttpStatusCode.ExpectationFailed) { indexHtml(false) }
             }
         }
